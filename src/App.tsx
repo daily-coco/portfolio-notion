5dfa2518d653a8a42d57d820082ca3984c368348
daily-coco <dailydev.sw@gmail.com>
2026-03-04 18:51:11 +0900

    feat: Router로 인한 Scroll Restoration 이슈 수정 - 브라우저의 높이값 초기화를 위한 ScrollToTop()추가

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,22 @@ import ProjectsPage from './pages/ProjectsPage';
 import ProjectDetailPage from './pages/ProjectDetailPage';
 import './styles/global.css';
 import { AnimatePresence } from 'framer-motion';
+import { useEffect } from 'react';
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  return null;
+}
 
 function App() {
   const location = useLocation();
   return (
     <>
+      <ScrollToTop />
       <AnimatePresence mode='wait'>
         <Routes location={location} key={location.pathname}>
           <Route path='/' element={<ProjectsPage />} />
