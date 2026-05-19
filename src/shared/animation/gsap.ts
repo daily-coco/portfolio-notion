c1e5ec44682711934b675796ccd0160596690383
daily-coco <dailydev.sw@gmail.com>
2026-05-20 07:07:20 +0900

    feat : vercel 배포 중 에러나는 ScrollReveal.tsx 에러 수정

--- src/shared/animation/gsap.ts
+++ src/shared/animation/gsap.ts
@@ -1,7 +1,6 @@
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
-import { useGSAP } from '@gsap/react';
 
-gsap.registerPlugin(ScrollTrigger, useGSAP);
+gsap.registerPlugin(ScrollTrigger);
 
 export { gsap, ScrollTrigger };
